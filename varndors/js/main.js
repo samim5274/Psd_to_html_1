acf93081d0a444988bec25fd7688cf62e62b4f19
Samim Hossen <85032804+samim5274@users.noreply.github.com>
2022-04-30 21:31:46 +0600

    update

--- varndors/js/main.js
+++ varndors/js/main.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
+
+
+  // STICKY MENU
+
+  $(".js--service-section").waypoint(function(direction){
+    if (direction == "down") {
+        $("nav").addClass("sticky");
+    }
+    else {
+      $("nav").removeClass("sticky");
+    }
+  });
+
+
+  // MIXITUP (PROTFOLIO SECTION)
   var mixer = mixitup('.container');
 });
