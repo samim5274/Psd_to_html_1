6f1d9c798e3faabf86700efca432b01ac39f269a
Samim Hossen <85032804+samim5274@users.noreply.github.com>
2022-05-10 13:17:28 +0600

    update

---
+++ resourse/js/main.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+
+
+  // STICKY MENU
+
+  $(".js--service-section").waypoint(function(direction){
+    if (direction == "down") {
+        $("nav").addClass("sticky");
+    }
+    else {
+      $("nav").removeClass("sticky");
+    }
+  });
+
+  // Active menu section
+
+  $("nav ul li a").click(function(){
+    $("nav ul li a").removeClass("active");
+    $(this).addClass("active");
+  });
+
+  $("nav a img.logo").click(function(){
+    $("nav ul li:first-child a").removeClass("active");
+    $(this).addClass("active");
+  });
+
+
+  // MIXITUP (PROTFOLIO SECTION)
+  var mixer = mixitup('.container');
+});
+
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// end
